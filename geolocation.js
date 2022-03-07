4bb4ced73624a67f18162b5bc44d9f5d78a6ab6d
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-03-07 23:32:40 +0530

    add geolocation, and other updates

---
+++ geolocation.js
@@ -0,0 +1,30 @@
+
+document.getElementById("current-location").addEventListener("click", FindMe);
+
+function FindMe() {
+  if (!navigator.geolocation) {
+    console.log("Geolocation is not supported by your browser");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+    console.log("Location is available");
+  }
+  function success(position) {
+    const data = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+    };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("/currentlocation", options).then((res) => {
+      console.log(res);
+    });
+  }
+  function error() {
+    console.log("Unable to retrieve your location");
+  }
+}
--- index.js
+++ index.js
@@ -1,48 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const https = require("https");
-const { isUndefined } = require("util");
 
-// initialize app express
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
+app.use(express.json({ limit: "1mb" }));
 app.set("view engine", "ejs");
 
-const apiKey = "f4c4753cee53daf25667f695906767e4";
+const apiKey = process.env.API_KEY;
 
-//get request for home route
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-//post request for home route
 app.post("/", function (req, res) {
-  let city = " ";
-  city = req.body.cityName;
-  let url;
-  console.log(city);
-  if (typeof city !== "undefined") {
-    url =
-      "https://api.openweathermap.org/data/2.5/weather?appid=" +
-      apiKey +
-      "&q=" +
-      city +
-      "&units=metric";
-  } else {
-    console.log("Current location button pressed");
-    res.sendFile(__dirname + "/error.html");
-  }
+  const city = req.body.cityName;
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&q=" +
+    city +
+    "&units=metric";
 
-  // const lat = position.coords.latitude;
-  // const long = position.coords.longitude;
-  // const url =
-  //   "https://api.openweathermap.org/data/2.5/weather?appid=" +
-  //   apiKey +
-  //   "&q=" +
-  //   city +
-  //   "&units=metric";
   https.get(url, function (response) {
     if (response.statusCode == 200) {
       response.on("data", function (data) {
@@ -78,6 +60,67 @@ app.post("/", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.get("/currentlocation", function (req, res) {
+  res.sendFile(__dirname + "/error.html");
+});
+
+app.post("/currentlocation", function (req, res) {
+  const latitude = req.body.lat;
+  const longitude = req.body.long;
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&units=metric";
+
+  https.get(url, function (response) {
+    if (response.statusCode == 200) {
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const city_Name = weatherData.name;
+        const country = weatherData.sys.country;
+        const city_Temp = Math.round(weatherData.main.temp);
+        const max_Temp = Math.round(weatherData.main.temp_max);
+        const min_Temp = Math.round(weatherData.main.temp_min);
+        const feels_Like = Math.round(weatherData.main.feels_like);
+        const humidity = weatherData.main.humidity;
+        const description = weatherData.weather[0].description;
+        let icon =
+          "http://openweathermap.org/img/wn/" +
+          weatherData.weather[0].icon +
+          "@2x.png";
+        //   console.log(city_Name);
+        //   console.log(country);
+        //   console.log(city_Temp);
+
+        // console.log(max_Temp);
+        // console.log(min_Temp);
+        // console.log(feels_Like);
+        // console.log(humidity);
+        // console.log(description);
+        // res.render("weather-data", {
+        //   city: city_Name,
+        //   temp: city_Temp,
+        //   country: country,
+        //   max_Temp: max_Temp,
+        //   min_Temp: min_Temp,
+        //   feels_Like: feels_Like,
+        //   humidity: humidity,
+        //   description: description,
+        //   icon: icon,
+        // });
+        res.sendFile(__dirname + "/error.html");
+      });
+    } else {
+      res.sendFile(__dirname + "/error.html");
+    }
+  });
+});
+
+const PORT = process.env.port || 3000;
+app.listen(PORT, function () {
   console.log("Server started at port 3000");
 });
